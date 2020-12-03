c9065a5fad2f0ed344eff1bc6f4f01d1ca43b87a
mateuswinter104 <mateuswinter2002@gmail.com>
2020-12-02 22:01:39 -0300

    4 - Operações

--- script.js
+++ script.js
@@ -1,26 +1,76 @@
 //variaveis de escopo global
-var num1, num2, op;
+var num1 = "", num2 = "", op = "";
 
-function teclarSom(){
+function teclarSom() {
     var bip = document.getElementById('bip');
 
     bip.volume = 1;
     bip.play();
-    
+
 }
 
-function pegarValorTecla(valor){ 
+function pegarValorTecla(valor) {
     //reproduz o som da tecla
     teclarSom();
-    //pega o valor da tecla
-    document.getElementById("display").innerHTML = valor;
+
+    //verifica se o operador  tem valor
+    if (op == "") {
+        num1 = num1 + valor;
+        atualizarDisplay(num1);
+    } else {
+        num2 = num2 + valor;
+        atualizarDisplay(num2);
+    }
+
 
 }
 
-function pegarOperador(){
+function pegarOperador(sinal) {
+    teclarSom();
+
+    if (num1 != "") {
+
+        if(num2 == "") {
+            op = sinal;
+            atualizarDisplay(num1 + sinal);
+
+        }else{
+            calcular();
+        }
+    }
+}
+
+function calcular() {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    var resultado = "";
+
+    if(op == "+"){
+        resultado = num1 + num2;
+    } else if(op == "-"){
+        resultado = num1 - num2;
+    }else if(op == "*"){
+        resultado = num1 * num2;
+    }else if(op == "/"){
+        resultado = num1 / num2;
+    }else if(op == "%"){
+        //resultado = 
+    }
+    atualizarDisplay(resultado);
+    limpar();
 
 }
 
-function calcular(){
+function atualizarDisplay() {
+    document.getElementById("display").innerHTML = "0";
+}
 
+function limpar() {
+    teclarSom();
+    atualizarDisplay(0);
+
+    num1 = "";
+    num2 = "";
+    op = "";
 }
